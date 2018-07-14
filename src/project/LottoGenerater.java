5b6841f59ea1ce8a79092f457cb974b10a411790
somyungsub <gkdldy5@naver.com>
2018-07-14 05:36:12 +0000

    lotto modify

--- src/project/LottoGenerater.java
+++ src/project/LottoGenerater.java
@@ -12,7 +12,7 @@ public class LottoGenerater {
         int[] result = new int[6];
         
         for(int i =0; i<6; i++){
-            result[i] = random.nextInt(45) + 1;
+            result[i] = random.nextInt(45)+1;
         }
         return result;
     }
